da99e76330e6336e92435a22529e7b04c253451d
Larry Hill <tomella1@gmail.com>
2020-05-20 22:12:17 +0930

    Update to use secrets manager to load reference data from PostGIS

--- deployment/build_reference_data.js
+++ deployment/build_reference_data.js
@@ -1,76 +1,83 @@
 /*
-   In the user running the Place Names it needs .bash_profile to set the following environment variables:
-
-   export PLACENAMES_DB_USER
-   export PLACENAMES_DB_HOST
-   export PLACENAMES_DB_DATABASE
-   export PLACENAMES_DB_PASSWORD
-   export PLACENAMES_DB_PORT
-
+   This runs only on an EC2 instance that has been registered to use AWS Secrets Manager to 
+   retrieve the PostGIS credentials and connection details. 
    Ask NLIG for credentials
 */
-
+const awsSecrets = require("../lib/awsSecrets");
 const { Client } = require('pg');
 
-const client = new Client({
-   user: process.env.PLACENAMES_DB_USER,
-   host: process.env.PLACENAMES_DB_HOST,
-   database: process.env.PLACENAMES_DB_DATABASE,
-   password: process.env.PLACENAMES_DB_PASSWORD,
-   port: process.env.PLACENAMES_DB_PORT,
- });
-
- client.connect((err, client, done) => {
-   // Handle connection errors
-   if (err) {
-      console.log(err);
-      return;
-   }
-
-   // SQL Query > Select Data
-   const query = client.query('select * from "PLACENAMES_FEATURE_CATALOGUE";')
-      .then(res => {
-         let groups = {};
-
-         res.rows.forEach(row => {
-            let groupName = row.group.trim();
-            let group = groups[groupName];
-            if(!group) {
-               group = groups[groupName] = {
-                  name: groupName,
-                  definition: row.group_definition
-               };
-            }
-
-            let categoryName = row.category.trim();
-            let category = group[categoryName];
-
-            if(!category) {
-               category = group[categoryName] = {
-                  name: categoryName,
-                  definition: row.category_definition,
-                  features:[]
-               };
-            }
-
-            let feature = {
-               name: row.feature.trim()
-            };
-            decorate(feature, row, 'definition', 'feature_definition');
-            decorate(feature, row, 'source', 'source');
-            decorate(feature, row, 'includedTerms', 'included_terms');
-            decorate(feature, row, 'examples', 'examples');
-
-            category.features.push(feature);
-
-         });
-         console.log(JSON.stringify(groups, null, 3));
-         process.exit();
-      });
+console.log("Starting read of postGis");
+bootstrap().then(response => {
+   console.log("Finit");
+   process.exit();
+}).catch(err => {
+   console.log("Finit with error", err);
+   process.exit();
 });
 
+async function bootstrap() {
+   const parameters = await awsSecrets(config.awsSecrets);
+
+   const client = new Client({
+      user: parameters.username,
+      host: parameters.host,
+      database: parameters.dbname,
+      password: parameters.password,
+      port: parameters.port,
+   });
+
+   client.connect((err, client, done) => {
+      // Handle connection errors
+      if (err) {
+         console.log(err);
+         return;
+      }
+
+      // SQL Query > Select Data
+      const query = client.query('select * from "PLACENAMES_FEATURE_CATALOGUE";')
+         .then(res => {
+            let groups = {};
+
+            res.rows.forEach(row => {
+               let groupName = row.group.trim();
+               let group = groups[groupName];
+               if (!group) {
+                  group = groups[groupName] = {
+                     name: groupName,
+                     definition: row.group_definition
+                  };
+               }
+
+               let categoryName = row.category.trim();
+               let category = group[categoryName];
+
+               if (!category) {
+                  category = group[categoryName] = {
+                     name: categoryName,
+                     definition: row.category_definition,
+                     features: []
+                  };
+               }
+
+               let feature = {
+                  name: row.feature.trim()
+               };
+               decorate(feature, row, 'definition', 'feature_definition');
+               decorate(feature, row, 'source', 'source');
+               decorate(feature, row, 'includedTerms', 'included_terms');
+               decorate(feature, row, 'examples', 'examples');
+
+               category.features.push(feature);
+
+            });
+            console.log(JSON.stringify(groups, null, 3));
+            process.exit();
+         });
+   });
+}
+
 function decorate(target, source, name, key) {
-   if(source[key]) {
+   if (source[key]) {
       target[name] = source[key];
    }
 }
